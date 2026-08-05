e4b02527f6297261e221cf29834bd4a5a5676c06
Jamel Almeida <jamelamleida1996@gmail.com>
2026-08-05 15:39:05 -0400

    Milestone 1 - Basic player movement

---
+++ Scripts/Characters/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var move_speed := 180.0
+
+func _physics_process(_delta):
+	var direction = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+
+	velocity = direction * move_speed
+	move_and_slide()
